1682716cb378e623698fef0e1eee2f209a8cd83f
Diego Carrion <dc.rec1@gmail.com>
2015-12-08 17:59:55 -0200

    feat: add Factory to dinamycally init the best app for a domain

--- lib/jobbie.rb
+++ lib/jobbie.rb
@@ -6,4 +6,16 @@ require 'jobbie/ninetynine_jobs'
 require 'jobbie/vagas'
 
 module Jobbie
+  class Factory
+    def self.init(params)
+      class_for(params[:url]).new params
+    end
+
+    def self.class_for(url)
+      { "www.vagas.com.br" => Vagas,
+        "www.infojobs.com.br" => InfoJobs,
+        "www.99jobs.com" => NinetynineJobs
+      }[URI.parse(url).host]
+    end
+  end
 end
--- spec/jobbie_spec.rb
+++ spec/jobbie_spec.rb
@@ -5,3 +5,16 @@ describe Jobbie do
     expect(Jobbie::VERSION).not_to be nil
   end
 end
+
+describe Jobbie::Factory do
+  describe ".init" do
+    { 'http://www.vagas.com.br/vagas/v1259656/tecnico-seguros-ramos-elementares' => Jobbie::Vagas,
+      'http://www.infojobs.com.br/vaga-de-programacao-php-em-santa-catarina__4795320.aspx' => Jobbie::InfoJobs,
+      'https://www.99jobs.com/abril/jobs/6645-estagio-em-comunicacao' => Jobbie::NinetynineJobs
+    }.each do |url, app_class|
+      it "inits #{app_class} for the URL #{url}" do
+        expect(described_class.init(url: url).class).to eql app_class
+      end
+    end
+  end
+end
